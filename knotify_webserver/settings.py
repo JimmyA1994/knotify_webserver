f287350b383d66a6788711ac976a7bb6886f7a61
JimmyA1994 <dimitrisadamis1994@gmail.com>
2022-03-20 17:48:09 +0200

    set up first unit test

--- knotify_webserver/settings.py
+++ knotify_webserver/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-84%t-8cm1$4@8%cg)0%-=(jvn5+%0d%mg5xm#=tb-$@g-#_zt(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'testserver']
 
 
 # Application definition
--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+class GetHomePageCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_homepage(self):
+        response = self.client.get('/')
+        assert response.status_code == 200
+        # test ideas: 
+        # * cross origin
