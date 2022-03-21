e436b5617bdd4103c468f87fa3d1360aeb5cebc3
JimmyA1994 <dimitrisadamis1994@gmail.com>
2022-03-22 00:19:27 +0200

    add knotify support

--- main/utils.py
+++ main/utils.py
@@ -1,8 +1,24 @@
+from logging.config import valid_ident
+from tokenize import String
+from knotify import knotify
+
 class KnotifyClient:
 
-    def __init__():
-        pass
+    def __init__(self):
+        self.options = knotify.new_options()
+        self.options(args=[])
+        self.algorithm, self.config = knotify.from_options(self.options)
 
-    def predict(sequence):
-        # call external program
-        pass
+    def predict(self, sequence):
+        if self.is_valid_input(sequence):
+            results = self.algorithm.get_results(sequence, **self.config)
+            result = results.loc[0]
+            return result.dot_bracket
+        else:
+            return None
+    
+    def is_valid_input(self, sequence):
+        if sequence and isinstance(sequence, str):
+            return set(sequence).issubset({'G', 'C', 'A', 'U'})
+        else:
+            return False
--- main/views.py
+++ main/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.views.generic import TemplateView
-import requests, json
+from .utils import KnotifyClient
 
 # Create your views here.
 class HomePageView(TemplateView):    
@@ -14,11 +13,8 @@ class ResultsView(TemplateView):
 
 def results_view(request):
     sequence = request.POST['sequence']
-    print(sequence)
-    x = requests.get('http://10.249.231.107:12345/sequence=' + sequence)
-    d = json.loads(x.text)
-    answer = d.get('answer', '')
-    print(f'{answer = }')
+    client = KnotifyClient()
+    answer = client.predict(sequence)
 
     context = {'sequence': sequence, 'answer': answer}
     return render(request, 'results.html', context)
