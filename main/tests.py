4ef6031d7377ceaed1a6d7338f14c0c2cf58e1b7
JimmyA1994 <dimitrisadamis1994@gmail.com>
2022-04-02 15:02:41 +0300

    update .gitignore

--- main/tests.py
+++ main/tests.py
@@ -10,3 +10,14 @@ class GetHomePageCase(TestCase):
         assert response.status_code == 200
         # test ideas: 
         # * cross origin
+
+# class ResultsCase(TestCase):
+#     def setUp(self):
+#         self.client = Client()
+#         self.sequence = 'AAAAAACUAAUAGAGGGGGGACUUAGCGCCCCCCAAACCGUAACCCC'
+    
+#     def test_sequence(self):
+#         right_anwser = ''
+#         response = self.client.post('/results/', {'sequence': self.sequence})
+#         assert response.status_code == 200
+#         assert
