6e37979af0f19ff8a37f052ba0960a86b2d83561
JimmyA1994 <dimitrisadamis1994@gmail.com>
2022-04-03 16:47:04 +0300

    make aesthetic and structureal changes to output result lines

--- static/js/results.js
+++ static/js/results.js
@@ -2,23 +2,19 @@ function init(){
     window['sequence'] = document.querySelector('#sequence').textContent;
     window['dot-bracket'] = document.querySelector('#dot-bracket').textContent;
     window['input-size'] = window['sequence'].length
-    window['count'] = 1;
     reportWindowSize()
 }
 
 
 function reportWindowSize() {
-    var seqElem = document.querySelector('#sequence');
-    var dotElem = document.querySelector('#dot-bracket');
-    console.log('height: ', window.innerHeight);
-    console.log('width: ', window.innerWidth);
-    console.log('sequence width: ', seqElem.offsetWidth)
-    const font = 13;
-    const len = font*seqElem.textContent.length
-    console.log(len);
-    n = Math.floor(seqElem.offsetWidth / font)
-    console.log('n: ',n);
+    var rootElem = document.querySelector('#root');
+    // console.log('height: ', window.innerHeight);
+    // console.log('width: ', window.innerWidth);
+    // console.log('sequence width: ', rootElem.offsetWidth)
+    const font = 15;
+    const n = Math.floor(rootElem.offsetWidth / font)
 
+    // results output root node
     root = document.querySelector('#root');
     // delete all existing nodes
     while(root.children.length > 0){
@@ -27,20 +23,43 @@ function reportWindowSize() {
 
     var count = 1;
     for(i=0; i<=window['input-size']; i+=n){
-        var seq_row = document.createElement('p');
-        seq_row.setAttribute("class", "results");
-        seq_row.setAttribute("id", "sequence" + (count > 1 ? '-'+count : ''))
-        seq_row.innerHTML = count + "> " + window['sequence'].slice(i, i+n);
-        var dot_row = document.createElement('p');
-        dot_row.setAttribute("class", "results");
-        dot_row.setAttribute("id", "dot-bracket" + (count > 1 ? '-'+count : ''))
-        dot_row.innerHTML = count + "> " + window['dot-bracket'].slice(i, i+n);
+        // create sequence line and append it
+        var seq_count = document.createElement('div');
+        seq_count.setAttribute("class", "counter results noselect")
+        seq_count.innerHTML = count + "> ";
+        var seq_content = document.createElement('div');
+        seq_content.setAttribute("class", "results");
+        seq_content.setAttribute("id", "sequence" + (count > 1 ? '-'+count : ''));
+        seq_content.innerHTML = window['sequence'].slice(i, i+n);
 
-        root.appendChild(seq_row);
-        root.appendChild(dot_row);
+        seq_div = document.createElement('div');
+        seq_div.setAttribute('class', 'responsive-line')
+        seq_div.setAttribute('id', 'seq_div' + (count > 1 ? '-'+count : ''))
+
+        seq_div.appendChild(seq_count);
+        seq_div.appendChild(seq_content);
+        root.appendChild(seq_div)
+
+        // create dot-bracket line and append it
+        var dot_count = document.createElement('div');
+        dot_count.setAttribute("class", "counter results noselect")
+        dot_count.innerHTML = count + "> ";
+        var dot_content = document.createElement('div');
+        dot_content.setAttribute("class", "results");
+        dot_content.setAttribute("id", "dot-bracket" + (count > 1 ? '-'+count : ''))
+        dot_content.innerHTML = window['dot-bracket'].slice(i, i+n);
+
+        dot_div = document.createElement('div');
+        dot_div.setAttribute('class', 'responsive-line')
+        dot_div.setAttribute('id', 'dot_div' + (count > 1 ? '-'+count : ''))
+
+        dot_div.appendChild(dot_count);
+        dot_div.appendChild(dot_content);
+        root.appendChild(dot_div)
+
+
         count++;
     }
-    window['count'] = count - 1;
 }
 
 window.onload = init;
