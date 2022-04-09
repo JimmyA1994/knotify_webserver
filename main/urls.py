62990b0ec727250186434da303709576c3f4fd1d
JimmyA1994 <dimitrisadamis1994@gmail.com>
2022-04-09 20:56:12 +0300

    add forna.js for displaying th result's structure

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import HomePageView, ResultsView, results_view
+from .views import HomePageView, ResultsView, results_view, css_redirect
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     # path('results/', ResultsView.as_view(), name='results'),
-    path('results/', results_view, name='results')
+    # fornac asks corresponding css in results directory, instead of static
+    path(r'results/css/fornac.css', css_redirect),
+    path('results/', results_view, name='results'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from .utils import KnotifyClient
 
 # Create your views here.
-class HomePageView(TemplateView):    
+class HomePageView(TemplateView):
     template_name = 'home.html'
 
 
@@ -12,9 +13,16 @@ class ResultsView(TemplateView):
 
 
 def results_view(request):
-    sequence = request.POST['sequence']
+    sequence = request.POST['sequence'].upper()
     client = KnotifyClient()
     answer = client.predict(sequence)
 
     context = {'sequence': sequence, 'answer': answer}
     return render(request, 'results.html', context)
+
+# serve css static file. Used only redirection
+def css_redirect(request):
+    test_file = open('static/css/fornac.css', 'r')
+    response = HttpResponse(content=test_file)
+    response['Content-Type'] = 'text/css'
+    return response
