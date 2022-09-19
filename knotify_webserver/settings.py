c7ae5b4ec257f1f80b1518b3e8b01775edea81f7
JimmyA1994 <dimitrisadamis1994@gmail.com>
2022-09-20 02:31:18 +0300

    add django redis cache

--- knotify_webserver/settings.py
+++ knotify_webserver/settings.py
@@ -69,6 +69,16 @@ TEMPLATES = [
     },
 ]
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://redis:6379',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
+
 WSGI_APPLICATION = 'knotify_webserver.wsgi.application'
 
 
