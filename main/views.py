ee7ac764aab2b84ae2f9d45b73b9229363256977
JimmyA1994 <dimitrisadamis1994@gmail.com>
2022-11-20 23:41:34 +0200

    use monospace on detailview

--- main/views.py
+++ main/views.py
@@ -242,7 +242,7 @@ def update_history_view(request):
         previous_runs_queryset = previous_runs_queryset.filter(completed__gt=completed)
     previous_runs_queryset = (previous_runs_queryset.select_related('result__sequence')
                                                     .values_list('id', 'result__sequence', 'result__structure', 'submitted', 'completed'))
-    date_format = lambda x : dateformat.format(x, 'Y-m-d H:i:s O e')
+    date_format = lambda x : dateformat.format(x, 'Y-m-d H:i:s e')
     previous_runs = [{'id':str(id), 'sequence':sequence, 'structure': structure,'submitted':date_format(submitted), 'completed':date_format(completed)}
                      for id, sequence, structure, submitted, completed in previous_runs_queryset]
     current_runs = [{'id':str(run[0]), 'sequence':run[1], 'submitted':date_format(run[2])} for run in current_runs_queryset]
