f9edc3e70b7059b0d3e0874a4ac4e62025376bd1
JimmyA1994 <dimitrisadamis1994@gmail.com>
2022-12-27 15:02:39 +0200

    fix host on  predict task

--- knotify_webserver/settings.py
+++ knotify_webserver/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-84%t-8cm1$4@8%cg)0%-=(jvn5+%0d%mg5xm#=tb-$@g-#_zt(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'web']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'django']
 
 
 # Application definition
