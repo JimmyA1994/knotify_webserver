c75dbaa50dcfee9b4b4312f1379fad9bf4a773d2
JimmyA1994 <dimitrisadamis1994@gmail.com>
2023-01-15 21:52:42 +0200

    login page redesign

--- static/js/login.js
+++ static/js/login.js
@@ -1,11 +1,11 @@
-function toggleLogin(){
-    console.log('toggle!!!');;
-}
 function signup(){
     var email = document.querySelector("#EmailInput").value;
     var password = document.querySelector("#PasswordInput").value;
-    if(!email || !password){
-        return;
+    const regex = /[\w\d_+.-]+@[\w]+[.][A-Za-z]+/g
+    const regex_match = email.match(regex)
+    const is_valid_email = regex_match && regex_match.length == 1 && regex_match[0] == email
+    if(!email || !password || !is_valid_email){
+        return false;
     }
 
     var url = 'signup/';
@@ -24,9 +24,31 @@ function signup(){
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        if(data.hasOwnProperty('status') && data['status']=='good'){
+        if(data.hasOwnProperty('status') && data['status']=='OK'){
             window.location.replace(window.location.protocol + '//' + window.location.host + '/');
+        }else {
+            var msg;
+            if (data['reason'] == 'invalid_email') {
+                return false; // let bootstrap validation handle it
+            }
+            else if (data['reason'] == 'user_could_not_be_created') {
+                msg = "User could not be created.";
+            }
+            else {
+                msg = "Something went wrong."
+            }
+            const error_msg = document.querySelector("#error-msg");
+            error_msg.innerHTML = msg;
+            const error_alert = document.querySelector("#error-alert");
+            error_alert.classList.remove('d-none');
+            return false;
         }
+    })
+    .catch(error => {
+        const error_msg = document.querySelector("#error-msg");
+        error_msg.innerHTML = "Something went wrong.";
+        const error_alert = document.querySelector("#error-alert");
+        error_alert.classList.remove('d-none');
     });
     return false; // ignore form submission, we handle navigation
 }
@@ -34,8 +56,11 @@ function signup(){
 function login(){
     var email = document.querySelector("#EmailInput").value;
     var password = document.querySelector("#PasswordInput").value;
-    if(!email || !password){
-        return;
+    const regex = /[\w\d_+.-]+@[\w]+[.][A-Za-z]+/g
+    const regex_match = email.match(regex)
+    const is_valid_email = regex_match && regex_match.length == 1 && regex_match[0] == email
+    if(!email || !password || !is_valid_email){
+        return false;
     }
 
     var url = 'login/';
@@ -54,35 +79,89 @@ function login(){
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        if(data.hasOwnProperty('status') && data['status']=='good'){
+        if(data.hasOwnProperty('status') && data['status']=='OK'){
             window.location.replace(window.location.protocol + '//' + window.location.host + '/');
+        }else {
+            var msg;
+            if (data['reason'] == 'invalid_email') {
+                return; // let bootstrap validation handle it
+            }
+            else if (data['reason'] == 'user_not_found') {
+                msg = "User was not found.";
+            }
+            else {
+                msg = "Something went wrong."
+            }
+            const error_msg = document.querySelector("#error-msg");
+            error_msg.innerHTML = msg;
+            const error_alert = document.querySelector("#error-alert");
+            error_alert.classList.remove('d-none');
+            return false;
         }
+    })
+    .catch(error => {
+        const error_msg = document.querySelector("#error-msg");
+        error_msg.innerHTML = "Something went wrong.";
+        const error_alert = document.querySelector("#error-alert");
+        error_alert.classList.remove('d-none');
     });
     return false; // ignore form submission, we handle navigation
 }
 
 function toggleLogin(){
+    // toggle the signup auxiliary prompt
     var loginSpan = document.querySelector('#toggle-login-span');
     loginSpan.style = "display:none";
     var signupSpan = document.querySelector('#toggle-signup-span');
     signupSpan.style = "display:visible";
 
-    var button = document.querySelector('#button');
-    button.value = "Log in!";
+    // change the title
+    const title = document.querySelector('#title');
+    title.innerHTML = 'Login';
+
+    // change the CTA Button text
+    var button = document.querySelector('#cta-button');
+    button.innerHTML = "Log in!";
+
+    // change the form action
     var form = document.querySelector('form');
-    form.onclick = login;
-    // button.onclick = login;
+    form.onsubmit = login;
+
+    // clear previous error messages on inputs
+    form.classList.remove('was-validated');
+
+    // remove error message
+    const error_alert = document.querySelector("#error-alert");
+    error_alert.classList.add('d-none');
 }
 
 function toggleSignup(){
+    // toggle the login auxiliary prompt
     var signupSpan = document.querySelector('#toggle-signup-span');
     signupSpan.style = "display:none";
     var loginSpan = document.querySelector('#toggle-login-span');
     loginSpan.style = "display:visible";
 
-    var button = document.querySelector('#button');
-    button.value = "Sign up!";
+    // change the title
+    const title = document.querySelector('#title');
+    title.innerHTML = 'Create an account';
+
+    // change the CTA Button text
+    var button = document.querySelector('#cta-button');
+    button.innerHTML = "Sign up!";
+
+    // change the form action
     var form = document.querySelector('form');
-    form.onclick = signup;
-    // button.onclick = signup;
+    form.onsubmit = signup;
+
+    // clear previous error messages on inputs
+    form.classList.remove('was-validated');
+
+    // remove error message
+    const error_alert = document.querySelector("#error-alert");
+    error_alert.classList.add('d-none');
+}
+
+function goToHome() {
+    window.location.replace(window.location.protocol + '//' + window.location.host + '/');
 }
